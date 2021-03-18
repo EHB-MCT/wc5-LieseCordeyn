c3302254b82f6d2f13e260e29a609c9cdd76e15c
Liese Cordeyn <liese.cordeyn@student.ehb.be>
2021-03-18 18:08:14 +0100

    finished part 1 (send message to API)

--- js/index.js
+++ js/index.js
@@ -2,14 +2,45 @@
 
 const messageSystem = {
   startFetching() {
+    setInterval(this.fetchMessages, 1000);
   },
 
   sendMessage(msg) {
-    // https://thecrew.cc/api/message/create.php?token=__TOKEN__ POST
+    fetch(`https://thecrew.cc/api/message/create.php?token=${userSystem.token}`, {
+      method: 'POST',
+      body: JSON.stringify({
+          message: msg
+      })
+    })
+    .then(response =>{
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
   },
-
   fetchMessages() {
-    // https://thecrew.cc/api/message/read.php?token=__TOKEN__ GET
+    fetch(`https://thecrew.cc/api/message/read.php?token=${userSystem.token}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const output = document.getElementById('output');
+        output.innerHTML = '';
+        data.forEach(message =>{
+          let htmlString = `
+          <div class="message">
+            <span class="by">${message.handle}</span>
+            <span class="on">${message.created_at}</span>
+            <p>${message.message}</p>
+        </div>`;
+  
+        const output = document.getElementById('output');
+        output.insertAdjacentHTML('beforeend', htmlString);
+        })
+      })
+
+    console.log("fetch");
   }
 };
 
@@ -19,18 +50,34 @@ const userSystem = {
 
   saveToken() {
     localStorage.setItem("token", this.token);
+    console.log(localStorage);
   },
 
   getToken() {
     return localStorage.getItem("token");
   },
 
-  logout() { 
+  logout() {
     localStorage.removeItem("token");
   },
 
   login(email, password) {
-    // https://thecrew.cc/api/user/login.php POST
+    fetch("https://thecrew.cc/api/user/login.php", {
+        method: 'POST',
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        userSystem.token = data.token;
+        this.saveToken();
+
+        display.render();
+      })
   },
 
   updateUser(password, handle) {
@@ -40,7 +87,40 @@ const userSystem = {
 
 const display = {
   initFields() {
-  }
-};
+    let form = document.getElementById('loginForm')
+    form.addEventListener('submit', this.submitHandler1);
 
-  display.initFields();
+    let sendForm = document.getElementById('messageForm');
+    sendForm.addEventListener('submit', this.submitHandler2)
+  },
+  submitHandler1(e) {
+    e.preventDefault();
+    const inputEmail = document.getElementById('emailField');
+    const email = inputEmail.value;
+    const inputPassword = document.getElementById('passwordField');
+    const password = inputPassword.value;
+
+    userSystem.login(email, password);
+  },
+  submitHandler2(e){
+    e.preventDefault();
+    const input = document.getElementById('messageField');
+    const text = input.value;
+    console.log(text);
+    messageSystem.sendMessage(text);
+  },
+  render() {
+    console.log("render");
+
+    if (userSystem.getToken()) {
+      userSystem.token = userSystem.getToken();
+      document.getElementById("loginWindow").style.display = "none";
+      messageSystem.startFetching();
+    } else {
+      console.log('Not logged in!');
+    }
+  }
+
+};
+display.render();
+display.initFields();
